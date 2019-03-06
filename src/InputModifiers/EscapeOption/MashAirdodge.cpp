c1084d2a92f036d40c5a28c784893d64cbb540d6
Michael Buciuman-Coman <michael.buciuman-coman@hitachivantara.com>
2019-03-05 20:38:53 -0500

    Changed MashAirdodge to not select a specific random direction when air-dodging

--- src/InputModifiers/EscapeOption/MashAirdodge.cpp
+++ src/InputModifiers/EscapeOption/MashAirdodge.cpp
@@ -15,11 +15,6 @@ void MashAirdodge::modifyInput(Gamecube_Data_t &dataToModify) {
     case State::NEUTRAL:
         dataToModify.report.l = 0;
         dataToModify.report.left = MIN_AXIS_VAL;
-        dataToModify.report.xAxis = AVG_AXIS_VAL;
-        dataToModify.report.yAxis = AVG_AXIS_VAL;
-#ifdef DEBUG
-        Serial.println(F("Inputting NEUTRAL"));
-#endif
         if (waitRemaining()) {
             return;
         }
@@ -28,29 +23,8 @@ void MashAirdodge::modifyInput(Gamecube_Data_t &dataToModify) {
     case State::AIRDODGE:
         dataToModify.report.l = 1;
         dataToModify.report.left = MAX_AXIS_VAL;
-        dataToModify.report.xAxis = storedXAxis;
-        dataToModify.report.yAxis = storedYAxis;
-#ifdef DEBUG
-        Serial.println(F("Inputting AIRDODGE"));
-#endif
         if (waitRemaining()) {
             return;
-        }
-
-        // determine random axis to set min/max value
-        bool useXAxis = (rand() % 2) == 0;
-        // randomly choose whether to set previous axis to min or max value
-        bool useMaxVal = (rand() % 2) == 0;
-
-        // set the other axis to a random value
-        if (useXAxis) {
-            storedXAxis = useMaxVal ? MAX_AXIS_VAL : MIN_AXIS_VAL;
-            storedYAxis =
-                rand() % (MAX_AXIS_VAL - MIN_AXIS_VAL) + MIN_AXIS_VAL + 1;
-        } else {
-            storedXAxis =
-                rand() % (MAX_AXIS_VAL - MIN_AXIS_VAL) + MIN_AXIS_VAL + 1;
-            storedYAxis = useMaxVal ? MAX_AXIS_VAL : MIN_AXIS_VAL;
         }
         nextState = State::NEUTRAL;
         break;
